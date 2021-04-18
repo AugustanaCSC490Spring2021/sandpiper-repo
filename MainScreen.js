124af1af0086db291d64a15ac8ebaf7742c935e8
dzwiener <danielzwiener14@augustana.edu>
2021-04-18 12:11:56 -0500

    Removed external font loading due to issues

--- MainScreen.js
+++ MainScreen.js
@@ -11,18 +11,10 @@ class MainScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isReady: false,
+      isReady: true,
     };
   }
 
-  async componentDidMount() {
-    await Font.loadAsync({
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-      ...Ionicons.font,
-    });
-    this.setState({ isReady: true });
-  }
 
   render() {
     return (
