d1be6d927045345cf89caea9b36853958573c59c
dzwiener <danielzwiener14@augustana.edu>
2021-04-20 16:52:42 -0500

    Fixed the font loading issue on both ios and android #16

--- App.tsx
+++ App.tsx
@@ -1,16 +1,43 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
+import { AppLoading } from 'expo';
+import { Container, Text } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MainScreen from './MainScreen.js';
 import ResourcePage from './ResourcePage.js';
 import Emergency from './EmergencyContactPage.js';
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
 
-class App extends React.Component {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
+  
+//This starter was included in the native-base docs: https://docs.nativebase.io/docs/GetStarted.html
+//But was broken so I found this answer from user 'Jose Paez': https://stackoverflow.com/questions/57066075/console-error-fontfamily-roboto-medium-is-not-a-system-font-and-has-not-been
+  async componentDidMount() {
+    await Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium' : require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
+
   render() {
+    if (!this.state.isReady) {
+     <View></View>
+   }
+
+
     return (
       <NavigationContainer>
         <Stack.Navigator>
