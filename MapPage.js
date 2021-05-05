9cf19951be484fffe1345e8667013c8028b0a29b
April <chitran17@augustana.edu>
2021-05-04 22:40:25 -0500

    added real time location tracking to map page

--- MapPage.js
+++ MapPage.js
@@ -1,85 +1,93 @@
-import { Container, Content, Text, Header, Button, StyleProvider, Card, View } from 'native-base';
-import * as React from 'react';
-import styles from './style.js';
-import 'react-native-gesture-handler';
-import MapView, {Marker} from 'react-native-maps';
-import * as Location from 'expo-location';
-//import * as TaskManager from "expo-task-manager";
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+import * as TaskManager from "expo-task-manager";
 
 const LOCATION_TASK_NAME = "background-location-task";
 
-//code snippet obtained from: https://docs.nativebase.io/docs/GetStarted.html
 class Map extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      location: {coords: { latitude: 37.78825, longitude: -122.4324}},
-      geocode: null,
-      errorMessage: "",
-      mapRegion: null,
-    }
-  }
-
-  componentDidMount() {
-    this.getLocationAsync();
-  }
-
-  handleMapRegionChange = mapRegion => {
-    this.setState({ mapRegion });
-  };
-
-
-  getLocationAsync = async () => {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      this.setState({
-        errorMessage: 'Permission to access location was denied',
-      });
-    }
-
-    let location = await Location.getCurrentPositionAsync({accuracy:Location.Accuracy.Highest});
-    const { latitude , longitude } = location.coords
-    this.getGeocodeAsync({latitude, longitude})
-    this.setState({ location: { latitude, longitude } });
-    this.setState({
-        mapRegion: {
-          latitude: latitude,
-          longitude: longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        },
-    });
-    
-  };
-
-
-
-    getGeocodeAsync = async (location) => {
-      let geocode = await Location.reverseGeocodeAsync(location)
-      this.setState({ geocode })
+      region: null,
+      error: '',
     };
+  }
 
-  render(){
-    const {location,geocode, errorMessage } = this.state
-    return (
-      
-      <View style={styles.containermap}>
-        <MapView
-          provider={MapView.PROVIDER_GOOGLE}
-          style={styles.map}
-          region={this.state.mapRegion}
-          onRegionChange={this.handleMapRegionChange}
-        >
-        {!!this.state.location.latitude && !!this.state.location.longitude && <MapView.Marker
-                        coordinate={{"latitude": this.state.location.latitude, "longitude": this.state.location.longitude}}
-                    />}
-        </MapView>
-        
-      </View>
+  async componentDidMount() {
+    // Asking for device location permission
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status === "granted") {
+      this._getLocationAsync();
+    } else {
+      this.setState({ error: "Locations services needed" });
+    }
+  }
+
+  _getLocationAsync = async () => {
     
+    // watchPositionAsync Return Lat & Long on Position Change
+    this.location = await Location.watchPositionAsync(
+      {
+        enableHighAccuracy: true,
+        distanceInterval: 1,
+        timeInterval: 10000
+      },
+      newLocation => {
+        let { coords } = newLocation;
+        console.log(coords);
+        let region = {
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+          latitudeDelta: 0.045,
+          longitudeDelta: 0.045
+        };
+        this.setState({ region: region });
+      },
+      error => console.log(error)
+    );
+    return this.location;
+  };
+
+  
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          initialRegion={this.state.region}
+          showsCompass={true}
+          showsUserLocation={true}
+          rotateEnabled={true}
+          ref={map => {
+            this.map = map;
+          }}
+          style={{ flex: 1 }}
+        />
+      </View>
     );
   }
 }
 
+TaskManager.defineTask(LOCATION_TASK_NAME, async ({ data, error }) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  if (data) {
+    const { locations } = data;
+    let lat = locations[0].coords.latitude;
+    let long = locations[0].coords.longitude;
+  }
+});
 
-export default Map;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  }
+});
+
+export default Map
